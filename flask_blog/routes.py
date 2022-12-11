e39b525be766c24d6d2d2cec0bdecb931493337e
kali <amr.ashraf19204@gmail.com>
2022-12-11 14:07:30 -0500

    Created the database

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+from flask import render_template, url_for, flash, redirect
+from flask_blog.forms import RegistrationForm, LoginForm
+from flask_blog.models import User, Post
+from flask_blog import app
 
 posts = [
     {
@@ -51,6 +50,3 @@ def login():
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
