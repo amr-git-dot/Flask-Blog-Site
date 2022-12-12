5f09a4e16f2d015b3b99a79c4f31890c03d2b16d
kali <amr.ashraf19204@gmail.com>
2022-12-12 15:36:12 -0500

    auth completed

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField,ValidationError
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from flask_blog.models import db,User
 
 
 class RegistrationForm(FlaskForm):
@@ -12,6 +13,16 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('user name already exist')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('email already exist')
 
 
 class LoginForm(FlaskForm):
